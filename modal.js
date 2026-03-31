b9b930fa78ddc84ab7f08d3223468ba3a973e1ac
Pedro Lucas Camara Rodrigues Lopes <10753510@educa.mackenzie.br>
2026-03-31 20:56:41 -0300

    feat: Demo entrar modal

---
+++ modal.js
@@ -0,0 +1,20 @@
+const modal = document.getElementById("modalAviso");
+const btnEntrar = document.getElementById("btnEntrar");
+const btnClose = document.getElementById("closeAviso");
+const btnOk = document.getElementById("btnOk");
+
+btnEntrar.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.style.display = "block";
+});
+
+const fecharModal = () => modal.style.display = "none";
+
+btnClose.onclick = fecharModal;
+btnOk.onclick = fecharModal;
+
+window.onclick = (event) => {
+    if (event.target == modal) {
+        fecharModal();
+    }
+};
